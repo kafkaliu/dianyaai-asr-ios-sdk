b85c339460be9cf48fdea7b6eb44ed7458f29379
Kafka Liu <kafka.liu@gmail.com>
2025-11-07 20:59:46 +0800

    For test

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let checksum = "ec19d9a1d9e8aa0742c5403d9308e3e3943af45b8befa102c48eb3bf60e5daed
 let url = "https://github.com/kafkaliu/dianyaai-asr-ios-sdk/releases/download/0.1.7/DianyaaiASR.xcframework.zip"
 
 let package = Package(
-    name: "DianyaaiASR",
+    name: "dianyaai-asr-ios-sdk",
     platforms: [
         .iOS(.v14)
     ],
