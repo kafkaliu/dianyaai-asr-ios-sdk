a883347bb57b9a85a3d2aef324fe588599b509dd
Kafka Liu <kafka.liu@gmail.com>
2025-11-07 20:45:23 +0800

    Fix: Remove dynamic type from Package.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-let version = "0.1.7"
+let version = "0.1.7.1"
 let checksum = "ec19d9a1d9e8aa0742c5403d9308e3e3943af45b8befa102c48eb3bf60e5daed"
 let url = "https://github.com/kafkaliu/dianyaai-asr-ios-sdk/releases/download/0.1.7/DianyaaiASR.xcframework.zip"
 
@@ -15,7 +15,6 @@ let package = Package(
     products: [
         .library(
             name: "DianyaaiASR",
-            type: .dynamic,
             targets: ["DianyaaiASR"]
         )
     ],
