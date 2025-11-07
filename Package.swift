58be3faac7c91a4d39e3be81bbb29eac0064da9a
Kafka Liu <kafka.liu@gmail.com>
2025-11-07 21:39:25 +0800

    For test

--- Package.swift
+++ Package.swift
@@ -3,31 +3,21 @@
 
 import PackageDescription
 
-let version = "0.1.11"
+let version = "0.1.12"
 let checksum = "ec19d9a1d9e8aa0742c5403d9308e3e3943af45b8befa102c48eb3bf60e5daed"
 let url = "https://github.com/kafkaliu/dianyaai-asr-ios-sdk/releases/download/0.1.7/DianyaaiASR.xcframework.zip"
 
 let package = Package(
-    name: "dianyaai-asr-ios-sdk",
+    name: "DianyaaiASR",
     platforms: [
         .iOS(.v14)
     ],
     products: [
-        .library(
-            name: "DianyaaiASR",
-            targets: ["DianyaaiASRWrapper"]
-        )
+        .library(name: "DianyaaiASR", targets: ["DianyaaiASR"])
     ],
-
     targets: [
-        .target(
-            name: "DianyaaiASRWrapper",
-            dependencies: [
-                .target(name: "DianyaaiASRBinary")
-            ]
-        ),
         .binaryTarget(
-            name: "DianyaaiASRBinary",
+            name: "DianyaaiASR",
             url: url,
             checksum: checksum
         )
